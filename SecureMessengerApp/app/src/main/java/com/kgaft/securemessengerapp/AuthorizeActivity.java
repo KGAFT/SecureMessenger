4ec81c9710aebc29e1b2ecfebd0f62f2e7e15d44
KGAFT <coldcool654@gmail.com>
2022-05-30 20:16:27 +0300

    Added login activity in app

---
+++ SecureMessengerApp/app/src/main/java/com/kgaft/securemessengerapp/AuthorizeActivity.java
@@ -0,0 +1,30 @@
+package com.kgaft.securemessengerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AuthorizeActivity extends AppCompatActivity {
+    private LoginFragment loginFragment;
+    private RegistrationFragment registrationFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorize);
+
+        loginFragment = new LoginFragment();
+        registrationFragment = new RegistrationFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.authorizeContainer, loginFragment).commitNow();
+    }
+
+    public void ocChangeToRegisterFragment(View view) {
+        getSupportFragmentManager().beginTransaction().remove(loginFragment).commitNow();
+        getSupportFragmentManager().beginTransaction().add(R.id.authorizeContainer, registrationFragment).commitNow();
+    }
+
+    public void ocChangeToLoginFragment(View view) {
+        getSupportFragmentManager().beginTransaction().remove(registrationFragment).commitNow();
+        getSupportFragmentManager().beginTransaction().add(R.id.authorizeContainer, loginFragment).commitNow();
+    }
+}
